a2f7aafd98fddcd24cdfd8d3032d6a8ae4246ded
Koen Hendriks <koenhendriks89@gmail.com>
2015-06-03 21:24:17 +0200

    Enables checking on Parse if there's a match between users

--- koen-BitDate/User.swift
+++ koen-BitDate/User.swift
@@ -66,11 +66,27 @@ func saveSkip(user: User){
 }
 
 func saveLike(user: User){
-	let like = PFObject(className: "Action")
-	like.setObject(PFUser.currentUser().objectId, forKey: "byUser")
-	like.setObject(user.id, forKey: "toUser")
-	like.setObject("liked", forKey: "type")
-	like.saveInBackgroundWithBlock(nil)
+	// check if us a user has already liked me
+	PFQuery(className: "Action")
+		.whereKey("byUser", equalTo: user.id)
+		.whereKey("toUser", equalTo: PFUser.currentUser().objectId)
+		.whereKey("type", equalTo: "liked")
+		.getFirstObjectInBackgroundWithBlock { (object, error) -> Void in
+			var matched = false
+			
+			if object != nil{
+				matched = true
+				object.setObject("matched", forKey: "type")
+				object.saveInBackgroundWithBlock(nil)
+			}
+			let match = PFObject(className: "Action")
+			match.setObject(PFUser.currentUser().objectId, forKey: "byUser")
+			match.setObject(user.id, forKey: "toUser")
+			match.setObject(matched ? "matched" : "liked", forKey: "type")
+			match.saveInBackgroundWithBlock(nil)
+		}
+	
+	
 }
 
 
